0571c8f29d7dbf8c513fc07e48153b27d7cea8e5
daneko <3a3k.web+github@gmail.com>
2017-07-06 16:44:32 +0900

    add subscribe method

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,6 @@
+/*
+https://firebase.google.com/docs/reference/functions/functions.pubsub の辺り見てもpublish「する」のはなさそう…?
+*/
 'use stric'
 
 const functions = require('firebase-functions');
@@ -5,22 +8,40 @@ const base64url = require('base64url');
 const PubSub = require('@google-cloud/pubsub')
 
 // 別プロジェクトのPUBSUBにPUBLISHする例
-// https trigger (ここではOmiseのWebhook) をうけとり、指定ProjectのPubsubへPUBLISH
+// https triggerをうけとり、指定ProjectのPubsubへPUBLISH
 // firebase projectのFirebaseサービスアカウントに、指定Project側のIAMにPUBSUBの権限付きで追加する必要が有ると思う
 exports.publishOtherProjectPubsub = functions.https.onRequest((request, response) => {
 	if(request.method === "POST"){
-		console.log("request body : %O", request.body);
-		var json = JSON.stringify(request.body);
-		var base64json = base64url(json);
-		console.log(base64json);
-		if(!base64json || 0 === base64json.length) {
-			response.status(400).end();
-		} else {
-			var instance = PubSub({
-				projectId: 'semiotic-karma-172203',
-				keyFilename: './firebase-admin-sdk-key.json'
-			});
-			instance.topic('hello_topic').publish(base64json, function(err, messageId){
+		jsonToBase64(request.body, (err, data) => {
+			if(err){
+				response.status(400).end();
+			} else {
+				var instance = PubSub({
+					projectId: 'semiotic-karma-172203',
+					keyFilename: './firebase-admin-sdk-key.json'
+				});
+				instance.topic('hello_topic').publish(base64json, function(err, messageId){
+					if(err){
+						console.log(err);
+						response.status(500).end();
+					} else {
+						response.status(200).send(messageId);
+					}
+				});
+			}
+		});
+	} else {
+		response.status(400).end();
+	}
+});
+
+
+// 同一Project内のPubsubにPushする
+exports.publishProjectPubsub = functions.https.onRequest((request, response) => {
+	if(request.method === "POST"){
+		if(request.body){
+			var instance = PubSub();
+			instance.topic('test-topic').publish(request.body, function(err, messageId){
 				if(err){
 					console.log(err);
 					response.status(500).end();
@@ -28,13 +49,37 @@ exports.publishOtherProjectPubsub = functions.https.onRequest((request, response
 					response.status(200).send(messageId);
 				}
 			});
+		} else {
+			response.status(400).end();
 		}
 	} else {
 		response.status(400).end();
 	}
 });
 
+// 同一Project内のPubsubにpublishされたものを受け取る
+// https://github.com/firebase/functions-samples/tree/master/quickstarts/pubsub-helloworld
+// これは結局push subscriberとして登録される
+// pubsub自体にログはなさそう？なので受信したことだけでもpublishするFunctionと並列に並べておけば良さそう（同一Project内なら）
+exports.pubsubEventTrigger = functions.pubsub.topic('test-topic').onPublish(event => {
+	const pubSubMessage = event.data;
+	console.log("row pubsub message : " + JSON.stringify(pubSubMessage));
+	const messageBody = pubSubMessage.data ? base64url.decode(pubSubMessage.data) : null;
+	console.log("get : " + JSON.stringify(messageBody));
+});
 
-//// 同一Project内のPubsubにPushする
-//exports.publishProjectPubsub = functions.https.onRequest((request, response) => {
-//});
+
+
+// callback = function(err, data)
+// js docの書き方忘れた
+function jsonToBase64(json, callback){
+	console.log("request json : " + json);
+	var jsonStr = JSON.stringify(json);
+	var base64json = base64url(jsonStr);
+	console.log(base64json);
+	if(!base64json || 0 === base64json.length) {
+		callback( new Error("convert error"), null);
+	} else {
+		callback( null, base64json);
+	}
+}
