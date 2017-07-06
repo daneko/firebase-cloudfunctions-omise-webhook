a14c92a25887892428c6c47900adbf42bc1c1e81
daneko <3a3k.web+github@gmail.com>
2017-07-06 11:50:11 +0900

    add comment

--- functions/index.js
+++ functions/index.js
@@ -4,27 +4,23 @@ const functions = require('firebase-functions');
 const base64url = require('base64url');
 const PubSub = require('@google-cloud/pubsub')
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	console.log("get trigger");
+// 別プロジェクトのPUBSUBにPUBLISHする例
+// https trigger (ここではOmiseのWebhook) をうけとり、指定ProjectのPubsubへPUBLISH
+// firebase projectのFirebaseサービスアカウントに、指定Project側のIAMにPUBSUBの権限付きで追加する必要が有ると思う
+exports.publishOtherProjectPubsub = functions.https.onRequest((request, response) => {
 	if(request.method === "POST"){
 		console.log("request body : %O", request.body);
-		var key = request.body['key']
 		var json = JSON.stringify(request.body);
 		var base64json = base64url(json);
-		console.log(key);
 		console.log(base64json);
-		if(!base64json || 0 === base64json.length || !key || 0 === key.length){
+		if(!base64json || 0 === base64json.length) {
 			response.status(400).end();
 		} else {
-			var data = {
-				key : key,
-				value : base64json
-			};
 			var instance = PubSub({
 				projectId: 'semiotic-karma-172203',
 				keyFilename: './firebase-admin-sdk-key.json'
 			});
-			instance.topic('hello_topic').publish(data, function(err, messageId){
+			instance.topic('hello_topic').publish(base64json, function(err, messageId){
 				if(err){
 					console.log(err);
 					response.status(500).end();
@@ -34,7 +30,11 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 			});
 		}
 	} else {
-		response.status(202).end();
+		response.status(400).end();
 	}
 });
 
+
+//// 同一Project内のPubsubにPushする
+//exports.publishProjectPubsub = functions.https.onRequest((request, response) => {
+//});
